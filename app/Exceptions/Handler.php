8d4dd0e3b4565362e4acf49969e53153827de99f
Shahnawaz <shahnawaz@ranoliaventures.com>
2023-11-23 22:56:55 +0530

    adding files

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Exceptions;
+
+use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * The list of the inputs that are never flashed to the session on validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     */
+    public function register(): void
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+    public function render($request, Exception|Throwable $e)
+    {
+        if ($e instanceof ModelNotFoundException) {
+            return response()->json(['error' => 'Data not found.']);
+        }
+  
+        return parent::render($request, $e);
+    }
+}
---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2024-2025</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        Developed By <a href="https://musheeda.com/"> Musheeda Solution</a>
+    </div>
+</footer>
